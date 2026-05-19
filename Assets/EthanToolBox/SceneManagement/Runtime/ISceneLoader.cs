846ecce6ce40853955465944a64fed932ab46011
Ethân D'alessandro <90325305+EthanDalessandro@users.noreply.github.com>
2026-05-19 20:57:28 +0200

    Fixed Error

--- Assets/EthanToolBox/SceneManagement/Runtime/ISceneLoader.cs
+++
@@ -1,8 +0,0 @@
-namespace EthanToolBox.Core.SceneManagement
-{
-    public interface ISceneLoader
-    {
-        void LoadSceneGroup(SceneGroup group);
-        void UnloadScene(string sceneName);
-    }
-}
